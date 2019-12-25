69290a16000daa3542f9469fbf7ab52b1ff7232a
shanzm <2351144690@qq.com>
2019-12-26 02:22:22 +0800

    2019年12月26日 02:21:56-create 关于.NET的缓存

--- 001MemoryCache/Form1.cs
+++ 001MemoryCache/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,34 @@ namespace _001MemoryCache
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //添加引用：System.Runtime.Caching
+            //新建一个缓存对象，使用默认的缓存对象
+            //MemoryCache是存入到程序进程的内存中的，程序重启之后就没了
+            MemoryCache memCache = MemoryCache.Default;
+            //缓存以键值对的形式存储，缓存的生命期是10s
+            memCache.Add("name", "shanzm", DateTimeOffset.Now.AddSeconds(10));
+
+
+            //在Asp.net中的HttpContext.Cache就是对MemoryCache的封装
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MemoryCache memCache = MemoryCache.Default;
+            //string mem = memCache["name"].ToString();
+            string name = (string)memCache["name"];
+            if (name == null)
+            {
+                MessageBox.Show("无缓存");
+            }
+            else
+            {
+                MessageBox.Show(name);
+            }
         }
     }
 }
